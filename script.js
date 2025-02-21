58d333e217b0f0a33ccaf1b6d09cfdc37fca2780
Barrinha Jr <161760018+MuriloDeSouzaSoaresLima@users.noreply.github.com>
2025-02-21 15:41:28 -0300

    caso alguém coloque a data de falecimento menor doq a de nascimento

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 function calcularTempo() {
+
     const nome = document.getElementById("nome").value;
     const nascimento = new Date(document.getElementById("nascimento").value);
     let falecimento = document.getElementById("falecimento").value;
     falecimento = falecimento ? new Date(falecimento) : new Date();
 
     const diferenca = falecimento - nascimento;
-    const dias = Math.floor(diferenca / (1000 * 60 * 60 * 24));
+
+    if (diferenca > 0) {
+        const dias = Math.floor(diferenca / (1000 * 60 * 60 * 24));
     const semanas = Math.floor(dias / 7);
     const meses = Math.floor(dias / 30.44);
     const anos = Math.floor(dias / 365.25);
@@ -17,4 +20,9 @@ function calcularTempo() {
         - ${semanas} semanas<br>
         - ${meses} meses<br>
         - ${anos} anos`;
+    } else {
+         document.getElementById("resultado").innerHTML = `
+         data de falecimento não pode ser menor que a data de nascimento.`;
+    }
+    
 }
